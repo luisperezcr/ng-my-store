6339590651ac63a6361232f2c165ab4a47eab519
Luis Pérez <luis.perez@geekdacity.com>
2022-05-31 13:51:26 -0600

    feat: Set up product list component

--- src/app/store/store-routing.module.ts
+++ src/app/store/store-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreComponent } from './components/store/store.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: StoreComponent
+    component: StoreComponent,
+    children: [
+      {
+        path: 'products',
+        component: ProductsListComponent,
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'products'
+      }
+    ]
   }
 ];
 
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './components/store/store.component';
 import { StoreRoutingModule } from './store-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
-
+const materialModules = [
+  MatToolbarModule
+];
 @NgModule({
   declarations: [
-    StoreComponent
+    StoreComponent,
+    ProductsListComponent
   ],
   imports: [
     CommonModule,
-    StoreRoutingModule
+    StoreRoutingModule,
+    materialModules
   ]
 })
 export class StoreModule { }
