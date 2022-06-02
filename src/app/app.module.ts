4161e2ff530885814df35586b6d8b5c46c67ee4c
Luis Pérez <luis.perez@geekdacity.com>
2022-06-02 10:11:27 -0600

    style: Remove unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-// import { NgxsModule } from '@ngxs/store';
-// import { ProductsState } from './state/products/products.state';
 
 @NgModule({
   declarations: [
